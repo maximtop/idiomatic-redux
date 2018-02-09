25a64fe3bc9751d7899268d4d8e40f3e4a3ed766
Topciu Maxim <maximtop@gmail.com>
2018-02-09 16:30:28 +0300

    12 - Wrapping dispatch() to log actions

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,13 +3,32 @@ import { createStore } from 'redux';
 import throttle from 'lodash/throttle';
 import { loadState, saveState } from './localStorage';
 
+const addLoggingToDispatch = (store) => {
+  const rawDispatch = store.dispatch;
+  return (action) => {
+    if (!console.group) {
+      return rawDispatch(action);
+    }
+    console.group(action.type);
+    console.log('%c prev state', 'color: gray', store.getState());
+    console.log('%c action', 'color: blue', action);
+    const returnValue = rawDispatch(action);
+    console.log('%c next state', 'color: green', store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
+  };
+};
+
 const configureStore = () => {
   const persistedState = loadState();
   const devtools = window.__REDUX_DEVTOOLS_EXTENSION__;
   const store = createStore(todoApp, persistedState, devtools && devtools());
+  if (process.NODE_ENV !== 'production') {
+    store.dispatch = addLoggingToDispatch(store);
+  }
   
   store.subscribe(throttle(() => {
-    saveState({todos: store.getState().todos});
+    saveState({ todos: store.getState().todos });
   }, 1000));
   
   return store;
