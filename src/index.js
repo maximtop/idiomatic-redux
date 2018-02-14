4f729802de8889bdf4f02f41dc08192f695dff3c
Maxim Topchu <maximtop@gmail.com>
2018-02-14 17:41:17 +0300

    14 - Fetching data on route change

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import Root from './components/Root';
 import configureStore from './configureStore';
-import { fetchTodos } from './api';
 
 const store = configureStore();
-
-fetchTodos('all').then(todos => console.log(todos));
 
 render(
   <Root store={store}/>,
